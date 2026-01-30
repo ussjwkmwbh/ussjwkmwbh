0034eecd4fb3e3df421a76167e34d58ec3a2d33d
ussjwkmwbh <162303617+ussjwkmwbh@users.noreply.github.com>
2026-01-30 23:45:25 +0530

    Update min_marks.py

--- min_marks.py
+++ min_marks.py
@@ -10,6 +10,6 @@ percent_added_by_hy = 30
 
 percentage_yet_obtained = (pt1_marks/max_marks_pt)*percent_added_by_pt + (hy_marks/max_marks_hy)*percent_added_by_hy + (pt2_marks/max_marks_pt)*percent_added_by_pt
 percentage_needed = 33 - percentage_yet_obtained
-marks_needed_in_final = percentage_needed *(8/5)
+marks_needed_in_final = percentage_needed *(max_marks_hy/50)
 print("Marks needed in final exam to pass: ",marks_needed_in_final)
 print("Percentage obtained so far: ",percentage_yet_obtained)
